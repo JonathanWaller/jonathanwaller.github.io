a75fc5f7f932e087d90722faf3686bd5bab0e779
JonathanWaller <waller.j27@gmail.com>
2023-04-11 22:41:34 -0400

    established

---
+++ src/types/map.ts
@@ -0,0 +1,13 @@
+export interface MapLocation {
+    id: number;
+    location: string;
+    company: string;
+    title: string;
+}
+
+export interface Coordinate {
+    id: number;
+    place: string;
+    lat: number;
+    long: number
+}
